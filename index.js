fa6ef2e7a9e436284fa01d1e5dc8ea074e45cb8e
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2023-11-26 02:55:01 +0600

    Added register contest api

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+// const stripe = require('stripe')(process.env.VITE_STRIPE_SECRET_KEY);
 require("dotenv").config();
-// var jwt = require("jsonwebtoken");
 
+
+// var jwt = require("jsonwebtoken");
 // Middle ware
 app.use(express.json());
 app.use(
@@ -16,7 +18,8 @@ app.use(
 );
 
 // MONGODB CONNECTION
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sk8jxpx.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sk8jxpx.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://0.0.0.0:27017`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -41,11 +44,36 @@ async function run() {
     })
 
     // get single by id
-    app.get('/contest/',async(req,res)=> {
-      const {id} = req.query
-      const result = await contestCollection.findOne({_id:new ObjectId(id)})
-      return res.send(result)
-    })
+    app.get('/contest/', async (req, res) => {
+      try {
+        const { id } = req.query;
+    
+        if (!id) {
+          return res.status(400).send('Contest ID is required');
+        }
+    
+        const result = await contestCollection.findOne({ _id: new ObjectId(id) });
+    
+        if (!result || !result.winnerID || result.winnerID.length === 0) {
+          return res.status(404).send('No contest found or no winner IDs available');
+        }
+    
+        const winnersIds = result?.winnerID?.map(id => new ObjectId(id));
+    
+        const foundDocuments = await userCollection.find({ _id: { $in: winnersIds } }).toArray();
+    
+        if (!foundDocuments || foundDocuments.length === 0) {
+          return res.status(404).send('No matching users found');
+        }
+    
+        return res.send({contest:result,winners:foundDocuments});
+      } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).send('Internal Server Error');
+      }
+    });
+
+    
 
 
 
@@ -58,7 +86,8 @@ async function run() {
 
 
 
-    // ================== User post api ==================
+    // ================== post api ==================
+    //user
     app.post('/createUser',async(req,res)=> {
       const {email} = req.query
       const user = req.body
@@ -70,6 +99,43 @@ async function run() {
       res.send(result)
     })
 
+
+    // =========== payment ==========
+    // // payment intent
+    // app.post("/create-payment-intent", async (req, res) => {
+    //   const { prizeMoney } = req.body;
+    //   const IntPrice = parseInt(prizeMoney)
+    //   const amount = IntPrice * 100
+
+    //   console.log('sent cart amount',amount)
+    //   // return
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount:amount,
+    //     currency:'usd',
+    //     payment_method_types:['card']
+    //   })
+
+    //   res.send({
+    //     clientSecret:paymentIntent.client_secret
+    //   })
+    // })    
+
+
+    // ================== Patch method ==================
+    // Increasing the new participant number
+    app.patch('/participateContest', async (req, res) => {
+
+        const { id } = req.body;
+        const existingContest = await contestCollection.findOne({_id:new ObjectId(id)})
+        const result = await contestCollection.updateOne(
+          { _id: new ObjectId(id)},
+          { $set: { participants: parseInt(existingContest?.participants) + 1} }
+        );
+    
+      res.send(result)
+    });
+    
+    
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
